9148b7bbc639a3b622e6eba80b9c3f572caf1a65
darshanpatwari27 <patwaridarshan27@gmail.com>
2026-06-04 21:17:19 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://stayfinder-backend-6s5m.onrender.com/api",
 });
 
 export default API;
